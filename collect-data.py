a5538a52e38099459c2962dad62c56ee4bd80fbf
dashingdiptu12345 <72452775+dashingdiptu12345@users.noreply.github.com>
2021-02-21 00:54:37 +0530

    Add files via upload

---
+++ collect-data.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 09:02:54 2021
+
+@author: Deepanshu Dutta
+"""
+
+import cv2
+import numpy as np
+import os
+
+# Create the directory structure
+if not os.path.exists("data"):
+    os.makedirs("data")
+    os.makedirs("data/train")
+    os.makedirs("data/test")
+    os.makedirs("data/train/Dog")
+    os.makedirs("data/train/Cat")
+    os.makedirs("data/test/Dog")
+    os.makedirs("data/test/Cat")
+
+    
+
+# Train or test 
+mode = 'test'
+directory = 'data/'+mode+'/'
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    # Simulating mirror image
+    frame = cv2.flip(frame, 1)
+    
+    # Getting count of existing images
+    count = {'Dog': len(os.listdir(directory+"/Dog")),
+             'Cat': len(os.listdir(directory+"/Cat"))
+                                                    }
+    
+    # Printing the count in each set to the screen
+    cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "Dog : "+str(count['Dog']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "Cat : "+str(count['Cat']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+
+    
+    # Coordinates of the ROI
+    x1 = int(0.5*frame.shape[1])
+    y1 = 10
+    x2 = frame.shape[1]-10
+    y2 = int(0.5*frame.shape[1])
+    # Drawing the ROI
+    # The increment/decrement by 1 is to compensate for the bounding box
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
+    # Extracting the ROI
+    roi = frame[y1:y2, x1:x2]
+    roi = cv2.resize(roi, (64, 64)) 
+ 
+    cv2.imshow("Frame", frame)
+    
+    #_, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+    #kernel = np.ones((1, 1), np.uint8)
+    #img = cv2.dilate(mask, kernel, iterations=1)
+    #img = cv2.erode(mask, kernel, iterations=1)
+    # do the processing after capturing the image!
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    _, roi = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+    cv2.imshow("ROI", roi)
+    
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == 27: # esc key
+        break
+    if interrupt & 0xFF == ord('0'):
+        cv2.imwrite(directory+'Dog/'+str(count['Dog'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('1'):
+        cv2.imwrite(directory+'Cat/'+str(count['Cat'])+'.jpg', roi)
+
+    
+cap.release()
+cv2.destroyAllWindows()
